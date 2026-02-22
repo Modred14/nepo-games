18f4f50a8c0dd132f112ac770237a8f996cf713d
unknown <favourdomirin@gmail.com>
2026-02-22 03:32:43 -0500

    update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -66,32 +66,35 @@ export default function Home() {
   return (
     <div>
       <Reveal>
-        <header className="px-20">
-          <div className="flex text-center py-4 font-semibold items-center text-gray-500 justify-between  border-b border-gray-300 w-full">
-            <div></div>
-            <div className="flex gap-9">
-              <div>
-                <a href="">Marketplace</a>
+       <header className="top-0 w-full z-1000">
+          {" "}
+          <div className="px-20  bg-white border-b border-gray-300 ">
+            <div className="flex text-center py-4 font-semibold  items-center text-[#808080] justify-between w-full">
+              <div></div>
+              <div className="flex gap-9">
+                <div>
+                  <a href="">Marketplace</a>
+                </div>
+                <div>
+                  <a href="">Pricing</a>
+                </div>
+                <div>
+                  <a href="">About Us</a>
+                </div>
+                <div>
+                  <a href="">Support</a>
+                </div>
               </div>
               <div>
-                <a href="">Pricing</a>
-              </div>
-              <div>
-                <a href="">About Us</a>
-              </div>
-              <div>
-                <a href="">Support</a>
+                <button className="py-2 px-3 rounded-md border mr-4  hover:bg-gray-200/50 duration-500 transition-all">
+                  Log In
+                </button>
+                <button className="py-2 px-3 rounded-md  hover:bg-blue-800 border border-blue-600/90 duration-500 transition-all bg-[#0000FF] text-white font-bold">
+                  Get Started
+                </button>
               </div>
             </div>
-            <div>
-              <button className="py-2 px-3 rounded-md border mr-4 hover:bg-gray-200 duration-500 transition-all">
-                Log In
-              </button>
-              <button className="py-2 px-3 rounded-md  hover:bg-blue-800 border border-blue-600/90 duration-500 transition-all bg-[#0000FF] text-white font-bold">
-                Get Started
-              </button>
-            </div>
-          </div>
+          </div>{" "}
         </header>
       </Reveal>
       <main>
@@ -106,7 +109,7 @@ export default function Home() {
               <p className="text-gray-500 font-medium text-3xl pt-7 text-center">
                 Redefining how premium gaming accounts are traded — securely and
                 transparently. Skip the grind.{" "}
-                <span className="font-bold text-[#0000FF]">
+                <span className="font-bold text-[#0000FF] italic">
                   Trade with confidence.
                 </span>
               </p>
@@ -118,13 +121,13 @@ export default function Home() {
                 </div>
                 <div className="flex justify-center">
                   {" "}
-                  <button className=" py-5 px-5 rounded-2xl border mr-2 hover:bg-blue-100/70 duration-500 transition-all text-lg">
+                  <button className=" py-5 px-5 rounded-2xl border mr-2 hover:bg-blue-200/90 bg-blue-100/70  duration-500 transition-all text-lg">
                     Sell your account
                   </button>
                 </div>
               </div>
             </div>
-            <div className="p-10 py-0">
+            <div className="p-20 ">
               <img src="/nepo-market.png" alt="" />
             </div>
           </div>
@@ -154,16 +157,27 @@ export default function Home() {
               All Your Games. One Secure Marketplace
             </p>
             <div className="w-full flex pt-4 items-center justify-center">
-              <p className="text-2xl text-center text-gray-900- max-w-300 px-3 pb-4">
+              <p className="text-2xl text-center text-gray-900- max-w-300 px-3 pb-7">
                 Built for serious gamers, Nepo Games provides escrow protection,
                 verified sellers, and seamless transfers — so every trade is
                 smooth, safe, and guaranteed.
               </p>
             </div>
-            <div className="grid grid-cols-3 text-2xl text-center gap-3 px-2">
-              <div className="rounded-2xl bg-[#FBF6F6] px-2">
-                <img src="" alt="" />
-                <p className="font-bold">Escrow-Protected Payments</p>
+            <div className="grid grid-cols-3 text-2xl text-center gap-3 px-2 ">
+              <div className="group rounded-2xl pb-10 bg-[#FBF6F6] px-2 border border-blue-100 hover:border-blue-700 transition-all duration-700">
+                <div className="p-15 pb-5">
+                  {" "}
+                  <div className="w-full  h-100 overflow-hidden rounded-3xl">
+                    <img
+                      className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-115"
+                      src="/transact.jpg"
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <p className="font-bold transition-all duration-500 group-hover:text-blue-700">
+                  Escrow-Protected Payments
+                </p>
                 <p>
                   Every transaction on our platform is secured with escrow
                   protection. The buyer’s funds are safely held until the
@@ -173,9 +187,21 @@ export default function Home() {
                   is always safe.
                 </p>
               </div>
-              <div className="rounded-2xl bg-[#FBF6F6]  px-2">
-                <img src="" alt="" />
-                <p className="font-bold">Advanced Search & Smart Filters</p>
+              <div className="group rounded-2xl pb-10 bg-[#FBF6F6] px-2 border border-blue-100 hover:border-blue-700 transition-all duration-700">
+                <div className="p-15 pb-5">
+                  {" "}
+                  <div className="w-full  h-100 overflow-hidden rounded-3xl">
+                    <img
+                      className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-115"
+                      src="/google.jpg"
+                      alt=""
+                    />
+                  </div>
+                </div>
+
+                <p className="font-bold transition-all duration-500 group-hover:text-blue-700">
+                  Advanced Search & Smart Filters
+                </p>
                 <p>
                   Find exactly what you’re looking for in seconds. Filter
                   accounts by game title, rank, skins, price range, platform,
@@ -183,9 +209,21 @@ export default function Home() {
                   listings and discover the best deals without wasting time.
                 </p>
               </div>
-              <div className="rounded-2xl bg-[#FBF6F6]  px-2">
-                <img src="" alt="" />
-                <p className="font-bold">Secure In-Platform Messaging</p>
+              <div className="group rounded-2xl pb-10 bg-[#FBF6F6] px-2 border border-blue-100 hover:border-blue-700 transition-all duration-700">
+                <div className="p-15 pb-5">
+                  {" "}
+                  <div className="w-full  h-100 overflow-hidden rounded-3xl">
+                    <img
+                      className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-115"
+                      src="message.jpg"
+                      alt=""
+                    />
+                  </div>
+                </div>
+
+                <p className="font-bold transition-all duration-500 group-hover:text-blue-700">
+                  Secure In-Platform Messaging
+                </p>
                 <p>
                   Communicate directly with sellers through our secure in-app
                   messaging system. Share details, confirm requirements, and
@@ -195,6 +233,9 @@ export default function Home() {
               </div>
             </div>
           </div>
+        </Reveal>
+        <Reveal>
+          <div className="pt-10"></div>
         </Reveal>
       </main>
     </div>
