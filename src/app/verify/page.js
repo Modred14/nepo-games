ed00294a0f2e4ca25706b924a3c4fbd740e42272
unknown <favourdomirin@gmail.com>
2026-03-10 07:11:21 -0400

    Fix deploy issue

--- src/app/verify/page.js
+++ src/app/verify/page.js
@@ -1,39 +1,10 @@
-"use client";
+import { Suspense } from "react";
+import VerifyClient from "./VerifyClient";
 
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-
-export default function VerifyPage() {
-  const params = useSearchParams();
-  const token = params.get("token");
-  const [message, setMessage] = useState("Verifying your email...");
-
-  useEffect(() => {
-    if (token) {
-      fetch(`/api/verify?token=${token}`)
-        .then((res) => res.json())
-        .then((data) => {
-          if (data.redirect) {
-            console.log("redirect")
-            window.location.href = data.redirect;
-            return; 
-          }
-
-          if (data.error) {
-            setMessage(data.error);
-          } else {
-            setMessage("Verification successful! Redirecting...");
-            setTimeout(() => {
-              window.location.href = "/login";
-            }, 2000);
-          }
-        })
-        .catch((err) => {
-          setMessage("Something went wrong");
-          console.log(err);
-        });
-    }
-  }, [token]);
-
-  return <p>{message}</p>;
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <VerifyClient />
+    </Suspense>
+  );
 }
