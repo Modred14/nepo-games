aee5ba34aca3c261d66917f059758c8bc9ec7fb6
unknown <favourdomirin@gmail.com>
2026-03-15 06:25:52 -0400

    Update loading state

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,13 +1,10 @@
 import PageLoader from "@/components/PageLoader";
 import LoginClient from "./LoginClient";
-import Reveal from "../reveal";
 
 export default function LoginPage() {
   return (
     <PageLoader>
-      <Reveal>
-        <LoginClient />
-      </Reveal>
+      <LoginClient />
     </PageLoader>
   );
 }
