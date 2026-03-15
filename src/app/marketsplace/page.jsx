dd5c52ff11eccf8df9b8dd09d5367fcf57519c98
unknown <favourdomirin@gmail.com>
2026-03-15 06:06:00 -0400

    Add loading state

--- src/app/marketsplace/page.jsx
+++ src/app/marketsplace/page.jsx
@@ -1,14 +1,17 @@
 "use client";
 
 import useAuthGuard from "../hooks/useAuthGuard";
+import PageLoader from "@/components/PageLoader";
 
 export default function Marketplace() {
   useAuthGuard();
 
   return (
-    <div>
-      <h1>Welcome to Marketplace</h1>
-      {/* protected content */}
-    </div>
+    <PageLoader>
+      <div>
+        <h1>Welcome to Marketplace</h1>
+        {/* protected content */}
+      </div>
+    </PageLoader>
   );
 }
