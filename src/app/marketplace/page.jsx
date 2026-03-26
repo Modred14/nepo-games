b3f8c49fdd07308e7b4ffcc7cec0c35a4fc9121d
unknown <favourdomirin@gmail.com>
2026-03-26 09:54:34 -0400

    Update marketplace

--- src/app/marketplace/page.jsx
+++ src/app/marketplace/page.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import useAuthGuard from "../hooks/useAuthGuard";
 import PageLoader from "@/components/PageLoader";
 import { useRouter } from "next/navigation";
-import { Search } from "lucide-react";
+import { Search, ShoppingBasket, ShoppingCart, Verified } from "lucide-react";
 
 export default function Marketplace() {
   useAuthGuard();
@@ -29,15 +29,71 @@ export default function Marketplace() {
       router.push("/login");
     }
   };
+  const games = [
+    {
+      id: "1",
+      user_id: "2",
+      title: "Call of Duty",
+      slug: "/1",
+      description: "This is an efootball account",
+      views_count: "1003",
+      price: "N 1,000",
+      currency: "NGN",
+      proof_image_url: "",
+      cover_image: "/call-of-duty.png",
+      platform: "Mobile",
+      status: "Active",
+      created_at: "10pm",
+      updated_at: "",
+      deleted_at: "",
+      verified: true,
+    },
+    {
+      id: "1",
+      user_id: "2",
+      title: "Call of Duty",
+      slug: "/1",
+      description: "This is an efootball account",
+      views_count: "1003",
+      price: "N 1,000",
+      currency: "NGN",
+      proof_image_url: "",
+      cover_image: "/call-of-duty.png",
+      platform: "Mobile",
+      status: "Active",
+      created_at: "10pm",
+      updated_at: "",
+      deleted_at: "",
+      verified: true,
+    },
+    {
+      id: "1",
+      user_id: "2",
+      title: "Call of Duty",
+      slug: "/1",
+      description: "This is an efootball account",
+      views_count: "1003",
+      price: "N 1,000",
+      currency: "NGN",
+      proof_image_url: "",
+      cover_image: "/call-of-duty.png",
+      platform: "Mobile",
+      status: "Active",
+      created_at: "10pm",
+      updated_at: "",
+      deleted_at: "",
+      verified: false,
+    },
+  ];
 
   return (
     <PageLoader>
       <div>
         <div className="flex border-b border-[#0000FF]/40 justify-between py-4 px-7 items-center">
-          <p className="text-xl font-bold">Hello, {user?.username}</p>
-          <div className="flex items-center gap-5">
+          <p className="text-lg sm:text-xl font-bold">Hello, {user?.username}</p>
+          <div className="flex items-center gap-2 sm:gap-5">
             {" "}
-            <div className="relative w-70">
+            <div className="relative w-30 sm:w-70 ">
               <Search
                 size={15}
                 className="absolute left-3 -mt-[0.25px] top-1/2 -translate-y-1/2 text-gray-500"
@@ -45,12 +101,12 @@ export default function Marketplace() {
 
               <input
                 type="search"
-                className="bg-gray-200 w-full p-2 pl-10 text-sm rounded-2xl border border-blue-600/40"
+                className="bg-gray-200 w-full p-2 pl-9 sm:pl-10 text-xs sm:text-sm rounded-2xl border border-blue-600/40"
                 placeholder="Search for gaming accounts"
               />
             </div>
-            <div>
-              <img src="" alt="" />
+            <div className="sm:block hidden">
+              
               <p className="text-sm bg-blue-500 text-gray-50 rounded-lg p-2 px-3">
                 Become a seller
               </p>
@@ -61,6 +117,57 @@ export default function Marketplace() {
             >
               <img src={user?.profile_image} alt="" />
             </button>
+          </div>
+        </div>
+        <div className="px-[2%]   w-full py-[5%] sm:py-[2%]">
+          <div className="grid grid-cols-1 xs:grid-cols-2 sm:flex gap-2 sm:gap-5 flex-wrap">
+            {games.map((game, index) => {
+              return (
+                <div key={index}>
+                  <div className="border-2 rounded-md border-[#4F8CFF] sm:w-70">
+                    {game.verified && (
+                      <div className="w-full flex justify-end pr-2">
+                        {" "}
+                        <div className="-mb-93">
+                          <div className="flex  relative mt-2 py-0.5 w-fit rounded-2xl border px-2 gap-1 bg-green-100 text-xs text-green-800 items-center">
+                            Verified Trader{" "}
+                            <Verified
+                              className="fill-green-600 text-green-100"
+                              size={16}
+                            />
+                          </div>
+                        </div>{" "}
+                      </div>
+                    )}{" "}
+                    <div className="w-full  h-full">
+                      <img
+                        src={game.cover_image}
+                        className="w-fit  rounded-t-[4.5px] "
+                        alt=""
+                      />
+                    </div>{" "}
+                    <div className="px-2 flex items-center py-3  justify-between">
+                      {" "}
+                      <div>
+                        <div className="text-[#0000FF] font-bold ">
+                          {game.title}
+                        </div>
+                        <div>{game.price}</div>
+                      </div>
+                      <a>
+                        <button className="flex  text-white p-1.5 rounded-lg border border-[#0038C9] bg-linear-to-b from-[#4F8CFF] to-[#8A38F5] b items-center gap-1 sm:text-sm text-xs">
+                          Buy{" "}
+                          <ShoppingCart
+                            size={14}
+                            className="text-white fill-white"
+                          />
+                        </button>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
