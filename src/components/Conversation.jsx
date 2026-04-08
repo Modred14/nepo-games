0b6507ce544383feaf71111e1649c3aa2377d33b
unknown <favourdomirin@gmail.com>
2026-04-08 15:18:02 -0400

    Add conversation page

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { useRef } from "react";
+import { Send, Star } from "lucide-react";
+
 export default function Conversation({ buyerId, GameDetails }) {
   function maskEmail(email) {
     const [localPart, domain] = email.split("@");
@@ -13,11 +16,75 @@ export default function Conversation({ buyerId, GameDetails }) {
 
     return `${start}***${end}@${domain}`;
   }
+  const inputRef = useRef(null);
 
   return (
-    <div className="min-h-screen w-full flex bg-cover bg-center">
-      <div></div>{" "}
-      <div className="relative min-h-screen w-full">
+    <div className="h-screen w-full overflow-hidden flex bg-cover bg-center">
+      <div className="p-4 md:grid hidden">
+        <div className="border border-blue-500/40 rounded-xl shadow-sm bg-white sm:min-w-60 lg:min-w-72 overflow-hidden">
+          {/* Header */}
+          <div className="px-4 py-3 border-b bg-blue-50">
+            <p className="font-semibold text-blue-700 text-center text-sm tracking-wide">
+              SELLER INFORMATION
+            </p>
+          </div>
+
+          {/* Seller Info */}
+          <div className="flex px-4 py-4 items-center justify-between">
+            <div>
+              <p className="text-xs text-gray-500">Username</p>
+              <p className="font-semibold text-[#0000FF]">"game.username"</p>
+            </div>
+
+            <img
+              src="{game.profile_image}"
+              className="w-11 h-11 rounded-full border-2 border-blue-500/60 object-cover"
+              alt="seller"
+            />
+          </div>
+
+          {/* Rating */}
+          <div className="px-4 pb-4 border-b">
+            <div className="flex items-center gap-1 mb-1">
+              {[...Array(5)].map((_, i) => (
+                <Star key={i} size={15} className="text-gray-300" />
+              ))}
+            </div>
+            <p className="text-xs text-gray-500">No ratings yet</p>
+          </div>
+
+          {/* Performance */}
+          <div className="px-4 py-4">
+            <p className="text-sm font-semibold text-[#0000FF] mb-3">
+              Seller Performance
+            </p>
+
+            <div className="flex justify-between items-center">
+              <div className="space-y-2 text-sm">
+                <p>
+                  <span className="text-[#0000FF]">Quality:</span>{" "}
+                  <span className="font-medium text-green-600">Excellent</span>
+                </p>
+                <p>
+                  <span className="text-[#0000FF]">Ratings:</span>{" "}
+                  <span className="font-medium text-yellow-600">Good</span>
+                </p>
+                <p>
+                  <span className="text-[#0000FF]">Response:</span>{" "}
+                  <span className="font-medium text-green-600">Fast</span>
+                </p>
+              </div>
+
+              <img
+                src="/Vector (1).png"
+                className="h-14 opacity-80"
+                alt="performance"
+              />
+            </div>
+          </div>
+        </div>
+      </div>{" "}
+      <div className="relative z-10 h-screen  flex flex-col w-full">
         <div
           className="absolute inset-0 bg-cover bg-center"
           style={{ backgroundImage: "url('/conversation.png')" }}
@@ -25,40 +92,208 @@ export default function Conversation({ buyerId, GameDetails }) {
 
         <div className="absolute inset-0 bg-white/80" />
 
-        <div className="relative z-10 ">
-          <div className="p-2 px-4">
-            <div className="p-2 px-5 rounded-3xl bg-blue-500/15 border border-blue-700/50">
-              <div className="flex gap-2 items-center">
-                <img
-                  src="/profile.png"
-                  alt="User Image"
-                  className="h-10 w-10 border border-gray-100 rounded-full overflow-hidden object-cover"
-                />
-                <div>
-                  <p className="sm:text-base text-sm font-semibold text-blue-700">
-                    UserName
-                  </p>
-                  <p className="sm:text-sm text-xs text-gray-800">
-                    {maskEmail("favourdomirin@gmail.com")}
-                  </p>
-                </div>{" "}
-              </div>
-              <div className="fixed bottom-0 left-0 w-full lg:left-[320px] lg:w-[calc(100%-320px)] p-3 sm:p-4 bg-transparent">
-                <div className="flex items-center gap-2 sm:gap-3 bg-white/90 backdrop-blur-md border rounded-full px-3 sm:px-4 py-2 sm:py-3 shadow-md">
-                  <div className="w-6 h-6 bg-blue-500 rounded-sm flex items-center justify-center text-white text-xs">
-                    N
-                  </div>
+        <div className="relative z-10 h-screen flex flex-col w-full overflow-hidden">
+          {/* background */}
+          <div
+            className="absolute inset-0 bg-cover bg-center"
+            style={{ backgroundImage: "url('/conversation.png')" }}
+          />
+          <div className="absolute inset-0 bg-white/80" />
 
-                  <input
-                    type="text"
-                    placeholder="Type your message"
-                    className="flex-1 outline-none bg-transparent text-sm"
+          {/* CONTENT WRAPPER */}
+          <div className="relative z-10 flex flex-col h-full">
+            {/* HEADER (NO FIXED) */}
+            <div className="p-4 shrink-0">
+              <div className="w-full p-2 px-5 rounded-3xl bg-blue-500/15 border backdrop-blur-sm border-blue-700/50">
+                <div className="flex gap-2 items-center">
+                  <img
+                    src="/profile.png"
+                    className="h-10 w-10 rounded-full object-cover"
                   />
-
-                  <button className="bg-blue-600 text-white rounded-full p-2 hover:bg-blue-700 transition">
-                    ➤
-                  </button>
+                  <div>
+                    <p className="text-sm font-semibold text-blue-700">
+                      UserName
+                    </p>
+                    <p className="text-xs text-gray-800">
+                      {maskEmail("favourdomirin@gmail.com")}
+                    </p>
+                  </div>
                 </div>
+              </div>
+            </div>
+
+            {/* MESSAGES (THIS IS THE KEY FIX) */}
+            <div className="flex-1 overflow-y-auto px-6 pb-4">
+              Հայերեն Shqip ‫العربية Български Català 中文简体 Hrvatski Česky
+              Dansk Nederlands English Eesti Filipino Suomi Français ქართული
+              Deutsch Ελληνικά ‫עברית हिन्दी Magyar Indonesia Italiano Latviski
+              Lietuviškai македонски Melayu Norsk Polski Português Româna
+              Pyccкий Српски Slovenčina Slovenščina Español Svenska ไทย Türkçe
+              Українська Tiếng Việt Lorem Ipsum "Neque porro quisquam est qui
+              dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+              "There is no one who loves pain itself, who seeks after it and
+              wants to have it, simply because it is pain..." What is Lorem
+              Ipsum? Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry. Lorem Ipsum has been the industry's standard
+              dummy text ever since the 1500s, when an unknown printer took a
+              galley of type and scrambled it to make a type specimen book. It
+              has survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum. Why do we use it? It is a long established fact that
+              a reader will be distracted by the readable content of a page when
+              looking at its layout. The point of using Lorem Ipsum is that it
+              has a more-or-less normal distribution of letters, as opposed to
+              using 'Content here, content here', making it look like readable
+              English. Many desktop publishing packages and web page editors now
+              use Lorem Ipsum as their default model text, and a search for
+              'lorem ipsum' will uncover many web sites still in their infancy.
+              Various versions have evolved over the years, sometimes by
+              accident, sometimes on purpose (injected humour and the like).
+              Where does it come from? Contrary to popular belief, Lorem Ipsum
+              is not simply random text. It has roots in a piece of classical
+              Latin literature from 45 BC, making it over 2000 years old.
+              Richard McClintock, a Latin professor at Hampden-Sydney College in
+              Virginia, looked up one of the more obscure Latin words,
+              consectetur, from a Lorem Ipsum passage, and going through the
+              cites of the word in classical literature, discovered the
+              undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
+              1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good
+              and Evil) by Cicero, written in 45 BC. This book is a treatise on
+              the theory of ethics, very popular during the Renaissance. The
+              first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes
+              from a line in section 1.10.32. The standard chunk of Lorem Ipsum
+              used since the 1500s is reproduced below for those interested.
+              Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum"
+              by Cicero are also reproduced in their exact original form,
+              accompanied by English versions from the 1914 translation by H.
+              Rackham. Where can I get some? There are many variations of
+              passages of Lorem Ipsum available, but the majority have suffered
+              alteration in some form, by injected humour, or randomised words
+              which don't look even slightly believable. If you are going to use
+              a passage of Lorem Ipsum, you need to be sure there isn't anything
+              embarrassing hidden in the middle of text. All the Lorem Ipsum
+              generators on the Internet tend to repeat predefined chunks as
+              necessary, making this the first true generator on the Internet.
+              It uses a dictionary of over 200 Latin words, combined with a
+              handful of model sentence structures, to generate Lorem Ipsum
+              which looks reasonable. The generated Lorem Ipsum is therefore
+              always free from repetition, injected humour, or
+              non-characteristic words etc. 5 paragraphs words bytes lists Start
+              with 'Lorem ipsum dolor sit amet...' Donate: If you use this site
+              regularly and would like to help keep the site on the Internet,
+              please consider donating a small sum to help pay for the hosting
+              and bandwidth bill. There is no minimum donation, any sum is
+              appreciated - click here to donate using PayPal. Thank you for
+              your support. Donate bitcoin: 16UQLq1HZ3CNwhvgrarV6pMoA2CDjb4tyF
+              Translations: Can you help translate this site into a foreign
+              language ? Please email us with details if you can help. There is
+              a set of mock banners available here in three colours and in a
+              range of standard banner sizes: BannersBannersBanners NodeJS
+              Python Interface GTK Lipsum Rails .NET The standard Lorem Ipsum
+              passage, used since the 1500s "Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+              nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+              occaecat cupidatat non proident, sunt in culpa qui officia
+              deserunt mollit anim id est laborum." Section 1.10.32 of "de
+              Finibus Bonorum et Malorum", written by Cicero in 45 BC "Sed ut
+              perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+              est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+              velit, sed quia non numquam eius modi tempora incidunt ut labore
+              et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
+              veniam, quis nostrum exercitationem ullam corporis suscipit
+              laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
+              vel eum iure reprehenderit qui in ea voluptate velit esse quam
+              nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
+              voluptas nulla pariatur?" 1914 translation by H. Rackham "But I
+              must explain to you how all this mistaken idea of denouncing
+              pleasure and praising pain was born and I will give you a complete
+              account of the system, and expound the actual teachings of the
+              great explorer of the truth, the master-builder of human
+              happiness. No one rejects, dislikes, or avoids pleasure itself,
+              because it is pleasure, but because those who do not know how to
+              pursue pleasure rationally encounter consequences that are
+              extremely painful. Nor again is there anyone who loves or pursues
+              or desires to obtain pain of itself, because it is pain, but
+              because occasionally circumstances occur in which toil and pain
+              can procure him some great pleasure. To take a trivial example,
+              which of us ever undertakes laborious physical exercise, except to
+              obtain some advantage from it? But who has any right to find fault
+              with a man who chooses to enjoy a pleasure that has no annoying
+              consequences, or one who avoids a pain that produces no resultant
+              pleasure?" Section 1.10.33 of "de Finibus Bonorum et Malorum",
+              written by Cicero in 45 BC "At vero eos et accusamus et iusto odio
+              dignissimos ducimus qui blanditiis praesentium voluptatum deleniti
+              atque corrupti quos dolores et quas molestias excepturi sint
+              occaecati cupiditate non provident, similique sunt in culpa qui
+              officia deserunt mollitia animi, id est laborum et dolorum fuga.
+              Et harum quidem rerum facilis est et expedita distinctio. Nam
+              libero tempore, cum soluta nobis est eligendi optio cumque nihil
+              impedit quo minus id quod maxime placeat facere possimus, omnis
+              voluptas assumenda est, omnis dolor repellendus. Temporibus autem
+              quibusdam et aut officiis debitis aut rerum necessitatibus saepe
+              eveniet ut et voluptates repudiandae sint et molestiae non
+              recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut
+              aut reiciendis voluptatibus maiores alias consequatur aut
+              perferendis doloribus asperiores repellat." 1914 translation by H.
+              Rackham "On the other hand, we denounce with righteous indignation
+              and dislike men who are so beguiled and demoralized by the charms
+              of pleasure of the moment, so blinded by desire, that they cannot
+              foresee the pain and trouble that are bound to ensue; and equal
+              blame belongs to those who fail in their duty through weakness of
+              will, which is the same as saying through shrinking from toil and
+              pain. These cases are perfectly simple and easy to distinguish. In
+              a free hour, when our power of choice is untrammelled and when
+              nothing prevents our being able to do what we like best, every
+              pleasure is to be welcomed and every pain avoided. But in certain
+              circumstances and owing to the claims of duty or the obligations
+              of business it will frequently occur that pleasures have to be
+              repudiated and annoyances accepted. The wise man therefore always
+              holds in these matters to this principle of selection: he rejects
+              pleasures to secure other greater pleasures, or else he endures
+              pains to avoid worse pains." help@lipsum.com Privacy Policy ·
+            </div>
+
+            {/* INPUT (NO FIXED) */}
+            <div className="p-4 shrink-0">
+              <div
+                onClick={() => inputRef.current?.focus()}
+                className="flex items-center gap-3  min-w-0
+          bg-white/80 backdrop-blur-xl 
+          border border-blue-700/50
+          rounded-full px-4 py-2
+          shadow-xs cursor-text"
+              >
+                <div className="w-9 h-9 rounded-full overflow-hidden border border-gray-200 shrink-0">
+                  <img
+                    src="/conversation.png"
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+
+                <input
+                  ref={inputRef}
+                  type="text"
+                  placeholder="Type a message..."
+                  className="flex-1 min-w-0 bg-transparent outline-none text-gray-700 xs:text-base text-sm"
+                />
+
+                <button
+                  onClick={(e) => e.stopPropagation()}
+                  className="inline-flex min-w-0 items-center justify-center w-10 h-10 rounded-full bg-blue-600 text-white hover:bg-blue-700 active:scale-95 transition-all duration-200 shadow-sm hover:shadow-md disabled:opacity-50 disabled:cursor-not-allowed"
+                >
+                  <Send size={18} />
+                </button>
               </div>
             </div>
           </div>
