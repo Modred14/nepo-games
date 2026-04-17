7c1e04c31a0d66c9204fa14f60f61c264552ce8f
unknown <favourdomirin@gmail.com>
2026-04-17 07:22:50 -0400

    update landing page

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -361,7 +361,7 @@ export default function Conversation({ gameId, receiverId }) {
         <div className="sm:grid w-full h-full sm:w-70 lg:w-90 ">
           <div className="border border-blue-500/30 shadow-md bg-white w-full sm:w-70 lg:w-90  overflow-hidden flex flex-col h-full">
             {/* HEADER */}
-            <div className="relative px-4 py-3 flex items-center border-b border-gray-500/40 bg-blue-50/60 backdrop-blur-sm">
+            <div className="relative px-4 py-3 flex items-center border-b border-gray-500/15 bg-blue-50/60 backdrop-blur-sm">
               <button
                 onClick={() => router.push("/marketplace")}
                 className="absolute left-2 font-bold rounded-full active:scale-90 transition-transform duration-150"
