a4c25ee91d901216cb1e4b51b4f49fe29f411857
unknown <favourdomirin@gmail.com>
2026-04-22 07:54:33 -0400

    Update pageLoader

--- src/components/PageLoader.js
+++ src/components/PageLoader.js
@@ -26,30 +26,29 @@ export default function PageLoader({ children }) {
     };
   }, []);
 
-  return (
-    <>
-      {!loaded && (
-        <div className="fixed min-h-screen inset-0 z-9999 flex items-center justify-center bg-white">
-          <div className="flex flex-col items-center gap-4">
-            <div className="bg-blue-700 p-3 rounded-2xl">
-              {" "}
-              <img
-                src="/logo.png"
-                alt="Nepogames"
-                className="w-16 animate-pulse"
-              />
-            </div>
+  if (!loaded) {
+    return (
+      <div className="fixed inset-0 z-9999 flex items-center justify-center bg-white">
+        <div className="flex flex-col items-center gap-4">
+          <div className="bg-blue-700 p-3 rounded-2xl">
+            {" "}
+            <img
+              src="/logo.png"
+              alt="Nepogames"
+              className="w-16 animate-pulse"
+            />
+          </div>
 
-            <p className="text-blue-700 font-semibold">Loading Nepogames<span className="loading-dots"></span></p>
+          <p className="text-blue-700 font-semibold">
+            Loading Nepogames<span className="loading-dots"></span>
+          </p>
 
-            <div className="w-40 h-1 bg-gray-200 rounded overflow-hidden">
-              <div className="h-full bg-blue-600 animate-loading w-1/2"></div>
-            </div>
+          <div className="w-40 h-1 bg-gray-200 rounded overflow-hidden">
+            <div className="h-full bg-blue-600 animate-loading w-1/2"></div>
           </div>
         </div>
-      )}
-
-      {children}
-    </>
-  );
+      </div>
+    );
+  }
+  return <>{children}</>;
 }
