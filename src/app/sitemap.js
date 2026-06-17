f0b0caba20381e011548bdc5750c17eba8228124
unknown <favourdomirin@gmail.com>
2026-06-17 09:40:16 -0400

    Add sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,32 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap() {
+  const baseUrl = "https://nepogames.com";
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/marketplace`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/tournament`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+  ];
+}
