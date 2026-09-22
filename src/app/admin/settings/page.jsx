f3c59db2322f68a24297307ffcceeaa4665fc449
unknown <favourdomirin@gmail.com>
2026-09-22 18:56:14 -0400

    update

--- src/app/admin/settings/page.jsx
+++ src/app/admin/settings/page.jsx
@@ -84,7 +84,6 @@ export default function AdminSettingsPage() {
         onSave={() => save("escrow_window_minutes")}
         saving={saving === "escrow_window_minutes"}
       />
-      
 
       <SettingRow
         label="Minimum withdrawal"
